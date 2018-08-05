e9d3374714b592d2bc2acfd5f87f9541425e53e0
ug-code <ugurgunes@windowslive.com>
2018-08-06 00:22:05 +0300

    init index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const Board  = require('./src/api/board/board');
+const Pinner = require('./src/api/pinner/pinner');
+
+const PinBot = {
+    Board: Board,
+    Pinner: Pinner,
+
+};
+
+module.exports = PinBot;
